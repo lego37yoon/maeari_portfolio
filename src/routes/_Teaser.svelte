047ab4db3cad333b131a013fc72fe75273308938
lego37yoon <lego37yoon@outlook.com>
2022-08-16 18:49:19 +0900

    Apply Vite .env

---
+++ src/routes/_Teaser.svelte
@@ -0,0 +1,167 @@
+<script>
+    import { onMount } from 'svelte';
+    import { fade } from 'svelte/transition';
+    import { fireApp } from "./backend.js";
+    import { getFirestore, onSnapshot, doc } from "firebase/firestore";
+    
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    
+    const data = getFirestore(fireApp);
+    
+    // Initialize variables
+    let currentNoticeCount = 1;
+    let maxNoticeCount = 2;
+    let teaserTitle="안녕하세요, 여러분!";
+    let teaserText="제 포트폴리오 웹사이트에 오신 걸 환영합니다."
+    let noticeArray;
+    let noticeChange = true;
+    let slideWorking = false;
+    let teaserNotice="현재 공지사항이 없습니다.";
+    let teaserLinkText="";
+    let teaserLink="";
+    let teaserBackground="linear-gradient(45deg, cadetblue, cornflowerblue)";
+
+    async function slideShow() {
+        for (let i = 0; i < noticeArray.length;) {
+            if (noticeChange) {
+                currentNoticeCount = i + 1;
+                teaserNotice = noticeArray[i].text;
+                teaserLinkText = noticeArray[i]["link-title"] + " >";
+                teaserLink = noticeArray[i].link;
+                teaserBackground = noticeArray[i].background;    
+                if (i == noticeArray.length - 1) {
+                    i = -1;
+                }
+                i++;
+            }
+            await delay(5000);
+        }
+    }
+
+    onSnapshot(doc(data, "teaser", "intro"), (teaserData) => {
+        teaserTitle = teaserData.data().title;
+        teaserText = teaserData.data().desc;
+    });
+
+    onSnapshot(doc(data, "teaser", "notice"), (noticeData) => {
+        noticeArray = noticeData.data().data;
+        maxNoticeCount = noticeArray.length;
+
+        if (!slideWorking) {
+            slideWorking = true;
+            slideShow();
+        }
+    });
+
+    function playPauseEvent() {
+        if (playNoticeButton.on) {
+            noticeChange = false;
+        } else {
+            noticeChange = true;
+        }
+    }
+
+    onMount(async () => {
+        await import("@material/mwc-icon")
+        await import("@material/mwc-icon-button-toggle");
+    });
+</script>
+
+{#key teaserNotice}
+<section id="teaserArea" style:background={teaserBackground}>
+    <section id="Notice">
+        <h1 class="teaserTitle">{teaserTitle}</h1>
+        <p class="teaserText normalBannerText">{teaserText}</p>
+        {#if maxNoticeCount > 0}
+        <p class="smallBannerText teaserText" in:fade><mwc-icon>announcement</mwc-icon>{teaserNotice}</p>
+        <p class="smallBannerText teaserText" in:fade>
+            <a class="teaserLink" href="{teaserLink}">{teaserLinkText}</a>
+        </p>
+        {/if}
+    </section>
+    {#if maxNoticeCount > 1}
+    <section id="teaserCount">
+        <h1 id="currentCount">
+            {#if currentNoticeCount < 10}0{/if}{currentNoticeCount}
+        </h1>
+        <p></p>
+        <h1 id="maxCount">
+            {#if maxNoticeCount < 10}0{/if}{maxNoticeCount}
+        </h1>
+        <mwc-icon-button-toggle id="playNoticeButton" onIcon="play_arrow" offIcon="pause" aria-label="teaser play or pause button" on:icon-button-toggle-change="{playPauseEvent}"></mwc-icon-button-toggle>
+    </section>
+    {/if}
+</section>
+{/key}
+
+<style>
+    .smallBannerText {
+        font-size: 1.0rem;
+    }
+
+    .normalBannerText {
+        font-size: 1.2rem;
+    }
+
+    /* teaser 공간 CSS */
+    #teaserArea {
+        color: white;
+        padding: 1rem;
+        display: flex;
+        justify-content: space-between;
+        word-break: keep-all;
+        border-bottom-right-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+
+    .teaserText {
+        font-weight: 300;
+        color: white;
+    }
+
+    .teaserLink {
+        color: white;
+        text-decoration: none;
+        margin: 0 5px 0 5px;
+        border-bottom: solid 2px white;
+    }
+    .teaserLink:visited {
+        text-decoration: none;
+        color: white;
+    }
+
+    .teaserTitle {
+        font-family: 'SUIT Variable', 'Noto Sans CJK KR Light', sans-serif;
+        font-size: 2rem;
+        font-weight: 200;
+    }
+
+    #teaserCount h1 {
+        font-size: 1.1rem;
+        text-align: end;
+    }
+
+    #maxCount {
+        color: gainsboro;
+    }
+
+    #teaserCount p {
+        height: calc(100% - 10rem);
+        border-right: 3px solid white;
+    }
+
+    #teaserCount mwc-icon-button-toggle {
+        margin-top: -1rem;
+        margin-right: -1rem;
+    }
+
+    /* navigation bar CSS */
+    section {
+        font-size: 1.5rem;
+    }
+
+    mwc-icon {
+        margin-right: 0.5rem;
+        --mdc-icon-size: 16px;
+    }
+</style>
