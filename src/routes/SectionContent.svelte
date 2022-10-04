18e4802fb9f187fa57ff5aa52f15070a3e83e7b1
lego37yoon <lego37yoon@outlook.com>
2022-10-05 00:38:07 +0900

    New: Shows list UI and contacts
    - "Contacts and others" contents provided with own UI
    - Other sections have a single temporary mwc-list, mwc-list-item with own custom

--- src/routes/SectionContent.svelte
+++ src/routes/SectionContent.svelte
@@ -8,9 +8,38 @@
     export let subMenuName;
     let ifLoaded = false;
     let noData = false;
+    let fetchedData = null;
+
+    if (mainMenu != "all") {
+        onSnapshot(doc(fireData, mainMenu, subMenu), (sectionData) => {
+            if (sectionData != undefined) {
+                ifLoaded = true;
+                fetchedData = sectionData.data();
+            } else {
+                noData = true;
+            }
+        });
+    } else {
+        //TODO: get other resources
+
+        onSnapshot(doc(fireData, "others", "contact"), (sectionData) => {
+            if (sectionData != undefined) {
+                ifLoaded = true;
+                if (fetchedData != null) {
+                    fetchedData = fetchedData.concat(sectionData.data());
+                } else {
+                    fetchedData = sectionData.data();
+                }
+            } else {
+                noData = true;
+            }
+        });
+    }
+    
 
     onMount(async() => {
         await import("@material/mwc-list");
+        await import("@material/mwc-icon");
     });
 
 </script>
@@ -18,19 +47,40 @@
 {#if noData === false}
     <h2>{subMenuName}</h2>
 {#if ifLoaded === true}
-    <mwc-list>
-        <mwc-list-item graphic="icon" twoline>
-
-        </mwc-list-item>
-        <li divider inset padded role="separator"></li>
-    </mwc-list>
+    {#if subMenu != "contact"}
+        <mwc-list>
+            <mwc-list-item graphic="icon" twoline>
+                <span>개발 중인 버전이어서 아직 구현되지 않았습니다.</span>
+                <span slot="secondary">조금만 더 기다려주시면 이 부분도 보실 수 있도록 개선할게요.</span>
+                <mwc-icon slot="graphic">warning</mwc-icon>
+            </mwc-list-item>
+            <li divider inset padded role="separator"></li>
+        </mwc-list>
+    {:else}
+        <p id="person_name">{fetchedData.name} a.k.a {fetchedData.nickname}</p>
+        <p class="person_info" id="person_email"><mwc-icon>email</mwc-icon><a href="mailto:{fetchedData.email}" target="_blank">{fetchedData.email} ↗</a></p>
+        <p class="person_info" id="person_github"><mwc-icon>code</mwc-icon><a href="https://github.com/{fetchedData.github}">GitHub ({fetchedData.github}) ↗</a></p>
+        <p class="person_info" id="person_blog"><mwc-icon>rss_feed</mwc-icon><a href="{fetchedData.blog}" target="_blank">Blog ↗</a></p>
+        <p class="person_info" id="person_twitter"><mwc-icon>forum</mwc-icon><a href="https://twitter.com/{fetchedData.twitter}" target="_blank">Twitter (@{fetchedData.twitter}) ↗</a></p>
+    {/if}
 {:else}
     <p>데이터를 불러오는 중입니다. 잠시만 기다려주세요.</p>
 {/if}
 {/if}
 
 <style>
-    mwc-list {
+    mwc-list-item {
         border-radius: 10px;
+    }
+    #person_name {
+        font-size: 1.4rem;
+        font-weight: 300;
+    }
+
+    .person_info a {
+        vertical-align: super;
+        margin: auto 5px auto 5px;
+        text-decoration: none;
+        color: cadetblue;
     }
 </style>
