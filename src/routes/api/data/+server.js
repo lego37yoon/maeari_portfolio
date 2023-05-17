79dab016bf31153bd0dd5f4cb2da4c56b97c1182
paperbox <lego37yoon@outlook.com>
2023-05-17 19:51:28 +0900

    func: fetch teaser data from Firestore
    * BREAKING CHANGES *
    - mfp backend no longer send entire Firebase App object to frontend
    - mfp now using Firebase Lite SDK, not full SDK
    
    * New *
    - Fetch data and return json by type parameter
    -- currently supported types: banner (previously "teaser" data), contacts (previously "others/contacts")
    - Throw error if type undefined
    
    * Fixed *
    - Fix crashes when Firebase App initialized before
    
    * Future Plans *
    - Will support other types from Firestore Database

--- src/routes/api/data/+server.js
+++ src/routes/api/data/+server.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { initializeFirestore } from "firebase/firestore";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { error, json } from '@sveltejs/kit';
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,7 +12,39 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-export const fireApp = initializeApp(firebaseConfig);
-export const fireData = initializeFirestore(fireApp, {
-    experimentalForceLongPolling: true,
-});
+const fireApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const store = getFirestore(fireApp);
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+    const type = url.searchParams.get("type");
+    
+    switch (type) {
+        case "banner": {
+            const banner = await getDocs(collection(store, "teaser"));
+            const bannerData = {};
+            banner.forEach((doc) => {
+                bannerData[doc.id] = doc.data();
+            });
+
+            return new json(bannerData);
+        }
+        case "projects": {
+            break;
+        }
+        case "resume": {
+            break;
+        }
+        case "contacts": {
+            const contacts = await getDocs(collection(store, "contact"));
+            const contactData = {};
+            contacts.forEach((doc) => {
+                contactData[doc.id] = doc.data();
+            });
+
+            return new json(contactData);
+        }
+        default:
+            throw error(400, "데이터 종류 설정이 잘못되었습니다. 관리자에게 문의하세요.");
+    }
+}
