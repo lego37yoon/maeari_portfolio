64da32180180566f38362f3c75ee6f06883e4aea
paperbox <lego37yoon@outlook.com>
2023-06-02 01:04:19 +0900

    Add under construction message

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -7,7 +7,15 @@
 </svelte:head>
 
 <main>
+    <section id="blog">
+        <h1>새로 작성한 글을 만나보세요</h1>
+        <p>준비 중이에요</p>
+    </section>
     
+    <section id="twitter">
+        <h1>트위터에서는 이런 소식을 안내하고 있어요</h1>
+        <p>준비 중이에요</p>
+    </section>
 </main>
 
 <style>
