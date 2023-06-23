129303a7a5ff8f0dc1e79f06d21569fefd52fb16
paperbox <lego37yoon@outlook.com>
2023-06-23 16:06:32 +0900

    func: Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<main>
+    <h1>{$page.status}</h1>
+    <p>{$page.error.message}</p>
+    <p>새로고침 이후에도 증상이 지속되면 연락하기 메뉴의 연락처로 오류를 제보해주세요. 감사합니다.</p>
+</main>
+
+<style>
+    main {
+        padding: 0 1rem 0 1rem;
+    }
+</style>
