2dd2a06ba19d039efe62a7a3f58ec7ae728ff26b
paperbox <lego37yoon@outlook.com>
2023-06-25 04:05:31 +0900

    tmp: new error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,10 +6,19 @@
     <h1>{$page.status}</h1>
     <p>{$page.error.message}</p>
     <p>새로고침 이후에도 증상이 지속되면 연락하기 메뉴의 연락처로 오류를 제보해주세요. 감사합니다.</p>
+    <p><a href={$page.url.origin}>홈으로 돌아가기</a></p>
 </main>
 
 <style>
+    p {
+        margin: 0.5em 0;
+    }
+
+    h1 {
+        margin: 0;
+    }
+
     main {
-        padding: 0 1rem 0 1rem;
+        padding: 1em;
     }
 </style>
