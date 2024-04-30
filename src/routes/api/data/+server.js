72cab8141e0b6ec23cf719bb5588881cc64ae8e3
paperbox <lego37yoon@outlook.com>
2024-04-30 23:22:22 +0900

    (init) migrate to Firebase Server App for SSR

--- src/routes/api/data/+server.js
+++ src/routes/api/data/+server.js
@@ -1,4 +1,4 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeServerApp, getApp } from "firebase/app";
 import { getFirestore, collection, getDocs, getDoc } from "firebase/firestore/lite";
 import { error, json } from '@sveltejs/kit';
 
@@ -6,13 +6,14 @@ const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-const fireApp = initializeApp(firebaseConfig);
+const firebaseSettings = {
+    automaticDataCollectionEnabled: false
+}
+
+const fireApp = initializeServerApp(firebaseConfig, firebaseSettings);
 const store = getFirestore(fireApp);
 
 /** @type {import('./$types').RequestHandler} */
